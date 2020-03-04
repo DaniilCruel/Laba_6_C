ff9faff3a751cad7521ccf7be2ede0a2c0457d82
DaniilCruel <DaniilCruel@users.noreply.github.com>
2020-03-04 20:30:36 +0300

    Fix Kirp

--- src/Laba_6/Danilin_8gr/Field.java
+++ src/Laba_6/Danilin_8gr/Field.java
@@ -5,70 +5,145 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
 @SuppressWarnings("serial")
 public class Field extends JPanel {
+
+    private double dragOffsetX;
+    private double dragOffsetY;
+
     // Флаг приостановленности движения
     private boolean paused;
+
     // Динамический список скачущих мячей
     private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
+    private ArrayList<Kirpizh> rect = new ArrayList<Kirpizh>(1);
     // Класс таймер отвечает за регулярную генерацию событий ActionEvent
-// При создании его экземпляра используется анонимный класс,
-// реализующий интерфейс ActionListener
+    // При создании его экземпляра используется анонимный класс,
+    // реализующий интерфейс ActionListener
     private Timer repaintTimer = new Timer(10, new ActionListener() {
         public void actionPerformed(ActionEvent ev) {
-// Задача обработчика события ActionEvent - перерисовка окна
+            // Задача обработчика события ActionEvent - перерисовка окна
             repaint();
         }
     });
     // Конструктор класса BouncingBall
     public Field() {
-// Установить цвет заднего фона белым
+        // Установить цвет заднего фона белым
         setBackground(Color.WHITE);
-// Запустить таймер
+        // Запустить таймер
         repaintTimer.start();
     }
+
     // Унаследованный от JPanel метод перерисовки компонента
     public void paintComponent(Graphics g) {
-// Вызвать версию метода, унаследованную от предка
+        // Вызвать версию метода, унаследованную от предка
         super.paintComponent(g);
         Graphics2D canvas = (Graphics2D) g;
-// Последовательно запросить прорисовку от всех мячей из списка
+        // Последовательно запросить прорисовку от всех мячей из списка
         for (BouncingBall ball: balls) {
             ball.paint(canvas);
         }
+        Graphics2D path = (Graphics2D) g;
+        for (Kirpizh kirp: rect) {
+            kirp.paint(path);;
+        }
+
     }
+
     // Метод добавления нового мяча в список
     public void addBall() {
-//Заключается в добавлении в список нового экземпляра BouncingBall
-// Всю инициализацию положения, скорости, размера, цвета
-// BouncingBall выполняет сам в конструкторе
+        //Заключается в добавлении в список нового экземпляра BouncingBall
+        // Всю инициализацию положения, скорости, размера, цвета
+        // BouncingBall выполняет сам в конструкторе
         balls.add(new BouncingBall(this));
+
     }
+
+    public void addKirp() {
+
+        rect.add(new Kirpizh());
+        addMouseMotionListener(new MouseMotionHandler());
+        addMouseListener(new MouseHandler());
+
+    }
+
     // Метод синхронизированный, т.е. только один поток может
-// одновременно быть внутри
-    public synchronized void pause() {
-// Включить режим паузы
+    // одновременно быть внутри
+
+
+    public  void pause1() {
+        // Включить режим паузы
         paused = true;
     }
+
     // Метод синхронизированный, т.е. только один поток может
-// одновременно быть внутри
+    // одновременно быть внутри
     public synchronized void resume() {
-// Выключить режим паузы
+        // Выключить режим паузы
         paused = false;
-// Будим все ожидающие продолжения потоки
+        // Будим все ожидающие продолжения потоки
         notifyAll();
     }
+
+
     // Синхронизированный метод проверки, может ли мяч двигаться
-// (не включен ли режим паузы?)
+    // (не включен ли режим паузы?)
     public synchronized void canMove(BouncingBall ball) throws
             InterruptedException {
-        if (paused) {
-// Если режим паузы включен, то поток, зашедший
-// внутрь данного метода, засыпает
+        if(paused)
             wait();
+    }
+
+
+    /*
+     * нажатие кнопки мыши — идентификатор MOUSE_PRESSED;
+     * отпускание кнопки мыши — идентификатор MOUSE_RELEASED;
+     * щелчок кнопкой мыши — идентификатор MOUSE_CLICKED (нажатие и отпускание не различаются);
+     * перемещение мыши — идентификатор MOUSE_MOVED;
+     * перемещение мыши с нажатой кнопкой — идентификатор MOUSE_DRAGGED;
+     * появление курсора мыши в компоненте — идентификатор MOUSE_ENTERED;
+     * выход курсора мыши из компонента — идентификатор MOUSE_EXITED.
+     *
+     *
+     */
+
+    public class MouseHandler extends MouseAdapter{
+
+        public void mousePressed(MouseEvent e) {
+            if ((e.getModifiers() & MouseEvent.BUTTON2_MASK) == 0)
+                if(Kirpizh.contains(e.getX(), e.getY())){
+
+                    dragOffsetX = e.getX()- Kirpizh.getX();
+                    dragOffsetY = e.getY()- Kirpizh.getY();
+                }
+            repaint();
         }
+
+
+    }
+
+
+    public class MouseMotionHandler implements MouseMotionListener{
+
+        public void mouseDragged(MouseEvent e){
+
+            Kirpizh.setPos(e.getX() - dragOffsetX, e.getY() - dragOffsetY);
+
+
+
+        }
+
+        public void mouseMoved(MouseEvent e){
+			/*if (isDragged)
+				rectangle.setPos(e.getX(), e.getY());*/
+        }
+
     }
 }
--- src/Laba_6/Danilin_8gr/MainFrame.java
+++ src/Laba_6/Danilin_8gr/MainFrame.java
@@ -1,6 +1,5 @@
 package Laba_6.Danilin_8gr;
 
-import java.awt.BorderLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
@@ -9,56 +8,65 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import java.awt.BorderLayout;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
+
     // Константы, задающие размер окна приложения, если оно
-// не распахнуто на весь экран
+    // не распахнуто на весь экран
     private static final int WIDTH = 700;
     private static final int HEIGHT = 500;
+
     private JMenuItem pauseMenuItem;
     private JMenuItem resumeMenuItem;
+
     // Поле, по которому прыгают мячи
     private Field field = new Field();
+
     // Конструктор главного окна приложения
     public MainFrame() {
         super("Программирование и синхронизация потоков");
         setSize(WIDTH, HEIGHT);
         Toolkit kit = Toolkit.getDefaultToolkit();
-// Отцентрировать окно приложения на экране
-        setLocation((kit.getScreenSize().width - WIDTH)/2,
-                (kit.getScreenSize().height - HEIGHT)/2);
-// Установить начальное состояние окна развѐрнутым на весь экран
+        // Отцентрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
+        // Установить начальное состояние окна развернутым на весь экран
+        // setExtendedState(MAXIMIZED_BOTH);
 
-        setExtendedState(MAXIMIZED_BOTH);
-// Создать меню
+        // Создать меню
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         JMenu ballMenu = new JMenu("Мячи");
         Action addBallAction = new AbstractAction("Добавить мяч") {
             public void actionPerformed(ActionEvent event) {
                 field.addBall();
-                if (!pauseMenuItem.isEnabled() &&
-                        !resumeMenuItem.isEnabled()) {
-// Ни один из пунктов меню не являются
-// доступными - сделать доступным "Паузу"
+                if (!pauseMenuItem.isEnabled()  && !resumeMenuItem.isEnabled()) {
+                    // Ни один из пунктов меню не являются
+                    // доступными - сделать доступным "Паузу"
                     pauseMenuItem.setEnabled(true);
                 }
             }
         };
+
         menuBar.add(ballMenu);
         ballMenu.add(addBallAction);
+
         JMenu controlMenu = new JMenu("Управление");
         menuBar.add(controlMenu);
-        Action pauseAction = new AbstractAction("Приостановить движение"){
+
+
+        Action pauseAction = new AbstractAction("Приостановить движение мячей"){
             public void actionPerformed(ActionEvent event) {
-                field.pause();
+                field.pause1();
                 pauseMenuItem.setEnabled(false);
                 resumeMenuItem.setEnabled(true);
             }
         };
+
         pauseMenuItem = controlMenu.add(pauseAction);
         pauseMenuItem.setEnabled(false);
+
         Action resumeAction = new AbstractAction("Возобновить движение") {
             public void actionPerformed(ActionEvent event) {
                 field.resume();
@@ -66,14 +74,27 @@ public class MainFrame extends JFrame {
                 resumeMenuItem.setEnabled(false);
             }
         };
+
         resumeMenuItem = controlMenu.add(resumeAction);
         resumeMenuItem.setEnabled(false);
-// Добавить в центр граничной компоновки поле Field
+        // Добавить в центр граничной компоновки поле Field
         getContentPane().add(field, BorderLayout.CENTER);
+
+
+        JMenu KirpMenu = new JMenu("Кирпич");
+        menuBar.add(KirpMenu);
+        Action addKirpAction = new AbstractAction("Добавить кирпич") {
+            public void actionPerformed(ActionEvent event) {
+                field.addKirp();
+            }
+        };
+        menuBar.add(KirpMenu);
+        KirpMenu.add(addKirpAction);
     }
+
     // Главный метод приложения
     public static void main(String[] args) {
-// Создать и сделать видимым главное окно приложения
+        // Создать и сделать видимым главное окно приложения
         MainFrame frame = new MainFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
